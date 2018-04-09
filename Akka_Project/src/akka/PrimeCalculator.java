96300996272b097cd60dd95aa07cd0747654e09a
yirian <yirian@yirian-HP-Pavilion-dv6500-Notebook-PC>
2018-04-09 17:30:44 -0400

    Create PrimeMaster class

--- Akka_Project/src/akka/PrimeCalculator.java
+++ Akka_Project/src/akka/PrimeCalculator.java
@@ -1,5 +1,24 @@
 package akka;
 
-public class PrimeCalculator {
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
 
+public class PrimeCalculator {
+	
+	public void calculate (int start, int end) {
+		int[]  range = {start,end};
+	//1st
+	ActorSystem actorSystem = ActorSystem.create("primeCalculator");
+	
+	ActorRef primeMaster = actorSystem.actorOf(Props.create(PrimeMaster.class, 10));
+	
+	primeMaster.tell (range, primeMaster);
+	
+	}
+	
+	public static void main (String[] args) {
+		PrimeCalculator primeCalculator = new PrimeCalculator();
+		primeCalculator.calculate (1,100);
+	}
 }
